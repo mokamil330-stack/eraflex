584254f260cc61db9aa95775323124238fa304af
mokamil330-stack <mokamil330@gmail.com>
2026-07-25 19:23:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,43 @@ index=0;
 document.getElementById("slide").src=images[index];
 
 },2500);
+
+const form=document.getElementById("orderForm");
+
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+let mobile=document.getElementById("mobile").value;
+
+let pin=document.getElementById("pincode").value;
+
+if(mobile.length!=10){
+
+alert("Enter Valid Mobile Number");
+
+return;
+
+}
+
+if(pin.length!=6){
+
+alert("Enter Valid Pincode");
+
+return;
+
+}
+
+// Google Sheets Integration Part-4 me aayega
+
+document.getElementById("popup").style.display="flex";
+
+form.reset();
+
+});
+
+function closePopup(){
+
+document.getElementById("popup").style.display="none";
+
+}
