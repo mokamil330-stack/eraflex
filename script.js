ff8f002e320d5a80a5d1adc3e8614592be262171
mokamil330-stack <mokamil330@gmail.com>
2026-07-25 19:19:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Landing Page Ready");
+const images=[
+
+"images/1.jpg",
+
+"images/2.jpg",
+
+"images/3.jpg",
+
+"images/4.jpg",
+
+"images/5.jpg"
+
+];
+
+let index=0;
+
+setInterval(()=>{
+
+index++;
+
+if(index>=images.length){
+
+index=0;
+
+}
+
+document.getElementById("slide").src=images[index];
+
+},2500);
