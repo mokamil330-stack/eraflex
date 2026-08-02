bb4d740b65d3d8b65ae24f4237cebd5c88930bc6
mokamil330-stack <mokamil330@gmail.com>
2026-08-02 18:45:57 +0530

    Create thankyou.js

---
+++ thankyou.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+
+setTimeout(function(){
+
+document.querySelector(".checkmark").style.transform="scale(1.15)";
+
+setTimeout(function(){
+
+document.querySelector(".checkmark").style.transform="scale(1)";
+
+},300);
+
+},500);
+
+}
